e2258a4c2bc06060e8728e620d8794f02ebdbe14
vamc1432 <vamshipk5@gmail.com>
2019-05-19 15:02:16 +0530

    1st commit by vamshi

---
+++ app/src/main/java/com/example/simple/Login.java
@@ -0,0 +1,26 @@
+package com.example.simple;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private Button login_Button;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        login_Button = (Button) findViewById(R.id.login_button);
+        login_Button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(Login.this,"login successful",Toast.LENGTH_SHORT).show();
+    }
+}
