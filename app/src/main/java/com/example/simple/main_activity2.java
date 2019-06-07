8cf8b1a31227dbc99590672eeec5703f33c6aac4
VAKITI ROHITH REDDY <36909903+romartin719@users.noreply.github.com>
2019-06-07 10:09:01 +0530

    menu commit

--- app/src/main/java/com/example/simple/main_activity2.java
+++ app/src/main/java/com/example/simple/main_activity2.java
@@ -1,6 +1,10 @@
 package com.example.simple;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
@@ -10,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -19,26 +24,72 @@ public class main_activity2 extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    //bottom navigation
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+    private MoreFragment moreFragment;
+    private MatchesFragment matchesFragment;
+    private HomeFragment homeFragment;
+    private static final String TAG="rohith";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
         //Bottom Navigation Bar
+        homeFragment=new HomeFragment();
+        matchesFragment=new MatchesFragment();
+        moreFragment=new MoreFragment();
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.nav_botm);
+        frameLayout=(FrameLayout)findViewById(R.id.main2_frame);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        bottomNavigationView.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.my_matches:
+                        bottomNavigationView.setItemBackgroundResource(R.color.cardview_light_background);
+                        setFragment(matchesFragment);
+                        return true;
+                    case R.id.more:
+                        bottomNavigationView.setItemBackgroundResource(R.color.cardview_dark_background);
+                        setFragment(moreFragment);
+                        return true;
+
+                        default:
+                            return  false;
+
+
+                }
+            }
+
+
+
+
+        });
+
+        //side Navigation Bar
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-      logOut=(Button)findViewById(R.id.logout);
+
       mAuth=FirebaseAuth.getInstance();
-      logOut.setOnClickListener(new Button.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-             mAuth.signOut();
-          }
-      });
+
 
     }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main2_frame,fragment);
+        fragmentTransaction.commit();
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -47,4 +98,6 @@ public class main_activity2 extends AppCompatActivity {
             return true;
         return super.onOptionsItemSelected(item);
     }
+
+
 }
